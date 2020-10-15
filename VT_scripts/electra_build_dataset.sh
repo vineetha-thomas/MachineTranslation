afb75a0a90688b1ebc3780b3d5a290ac286fe621
Vineetha Thomas <avnin281@gmail.com>
2020-10-15 03:17:13 -0700

    Updating path

--- VT_scripts/electra_build_dataset.sh
+++ VT_scripts/electra_build_dataset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 ../build_pretraining_dataset.py \
+python3 ../electra/build_pretraining_dataset.py \
 --corpus-dir ../data/corpus_dir/combined/ \
 --vocab-file ../data/wordpiece_vocab.txt \
 --output-dir ../output/
