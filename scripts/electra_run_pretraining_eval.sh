465c90fda53f4668c71530b205bdcf9cb2d7454a
Vineetha Thomas <avnin281@gmail.com>
2020-11-24 02:38:39 -0800

    Updating eal file

--- scripts/electra_run_pretraining_eval.sh
+++ scripts/electra_run_pretraining_eval.sh
@@ -3,6 +3,6 @@ source ~/.bashrc
 conda activate nmt-gpu
 
 python3 ../electra/run_pretraining.py \
---data-dir ../data_small \
+--data-dir ../data_small/eval_dir \
 --model-name nmt_model1 \
 --hparams electra_config_eval.json
